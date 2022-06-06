1955e073dfb5c63d179feffd244a1d5690134998
nada <nada.mohamed@extech.com>
2022-06-06 14:23:45 +0200

    customers module namespace, controller test

---
+++ app/Modules/customers/Http/Controllers/ProductController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Customers\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+//use App\Models\User;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        dd('Helle from the product controller insde the customers\' controllers !');
+        // $products = Product::where('category_id', '!=', null)
+        //     ->whereHas('category', function ($query) {
+        //         $query->where('deleted_at', '=', null);
+        //     })->paginate(config('global.defaultPagination'));
+
+        // $data = [
+        //     'title' => 'Products',
+        //     'products' => $products,
+        //     'categories' =>
+        //     DB::select(DB::raw('SELECT * FROM categories where deleted_at is null')),
+        // ];
+
+        // return view('user.pages.products.index', $data);
+    }
+}
